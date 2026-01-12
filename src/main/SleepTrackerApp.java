9597c3f9767601a45170bb7376c46a8af678d42c
Sergei <ssnefedov97@mail.ru>
2026-01-12 19:48:25 +0300

    Устранение замечаний

--- src/main/SleepTrackerApp.java
+++ src/main/SleepTrackerApp.java
@@ -20,37 +20,29 @@ public class SleepTrackerApp{
     public static void main(String[] args)  throws IOException {
         FileWriter fw = new FileWriter("log.txt",false);
 
-        Logger.log("Начало лога");
-        List<SleepTrackerRecord> listSession = new ArrayList<>();
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yy HH:mm");
-        FileReader fileReader = new FileReader("sleep_log.txt");
-        BufferedReader br = new BufferedReader(fileReader);
-
-
-        while (br.ready()){
-            String line = br.readLine();
-            String[] lineToQuality = line.split(";");
-            try {
-                LocalDateTime start = LocalDateTime.parse(lineToQuality[0], formatter);
-                LocalDateTime end = LocalDateTime.parse(lineToQuality[1], formatter);
-                SleepQuality sq = SleepQuality.UNDEFINED;
-                SleepTrackerRecord sleepTrackerRecord = new SleepTrackerRecord(start,end,sq);
-                listSession.add(sleepTrackerRecord);
-            } catch (Exception e){
-                System.out.println("Ошибка при обработке строки: " + line);
-                System.out.println("Ошибка: " + e.getMessage());
-            }
+        if(args.length == 0){
+            System.out.println("Пожалуйста, укажите путь к файлу логов как аргумент командной строки");
         }
-        br.close();
+        String logFilePath = args[0];
 
-        //System.out.println(listSession);
+        if (!java.nio.file.Files.exists(java.nio.file.Path.of(logFilePath))) {
+            System.out.println("Файл не найден: " + logFilePath);
+            return;
+        }
+
+        ReadSleepSessionsFromFile readSleepSessionsFromFile = new ReadSleepSessionsFromFile();
+        List<SleepTrackerRecord> listSession = new ArrayList<>(readSleepSessionsFromFile.readSleepSessions("sleep_log.txt"));
+
+        Logger.log("Начало лога");
+
+
+
 
 
         System.out.println("________________ SleepAnalysisResult, SleepQualityResult");
         //SleepQualityResult
         SleepQualityResult sleepQualityResult = new SleepQualityResult();
         SleepAnalysisResult sleepAnalysisResult1 = sleepQualityResult.apply(listSession);
-        //System.out.println(sleepAnalysisResult1);
         Logger.log(" " + sleepAnalysisResult1);
 
 
@@ -59,14 +51,12 @@ public class SleepTrackerApp{
         NightsCount nightsCount = new NightsCount();
         //NightsCount
         SleepAnalysisResult sleepAnalysisResult2 = nightsCount.apply(listSession);
-        //System.out.println(sleepAnalysisResult2);
         Logger.log("Общее" + sleepAnalysisResult2);
 
         System.out.println("________________ SleepAnalysisResult, BadSession");
         //BadSession
         BadSession badSession1 = new BadSession();
         SleepAnalysisResult sleepAnalysisResult3 = badSession1.apply(listSession);
-        //System.out.println(sleepAnalysisResult3);
         Logger.log(" " + sleepAnalysisResult3);
 
 
@@ -74,33 +64,28 @@ public class SleepTrackerApp{
         //MaxTimeForSession
         MaxTimeForSession maxTimeForSession = new MaxTimeForSession();
         SleepAnalysisResult sleepAnalysisResult4 = maxTimeForSession.apply(listSession);
-        //System.out.println(sleepAnalysisResult4);
         Logger.log(" " + sleepAnalysisResult4);
 
         System.out.println("________________ SleepAnalysisResult, MinTimeForSession");
         //MinTimeForSession
         MinTimeForSession minTimeForSession = new MinTimeForSession();
         SleepAnalysisResult sleepAnalysisResult5 = minTimeForSession.apply(listSession);
-        //System.out.println(sleepAnalysisResult5);
         Logger.log(" " + sleepAnalysisResult5);
 
         System.out.println("________________ SleepAnalysisResult, MidleTimeForSession");
         //MidleTimeForSession
         MidleTimeForSession midleTimeForSession = new MidleTimeForSession();
         SleepAnalysisResult sleepAnalysisResult6 = midleTimeForSession.apply(listSession);
-        //System.out.println(sleepAnalysisResult6);
         Logger.log(" " + sleepAnalysisResult6);
 
         System.out.println("________________ SleepAnalysisResult, OutputInfo");
         OutputInfo outputInfo = new OutputInfo();
         SleepAnalysisResult sleepAnalysisResult7 = outputInfo.apply(listSession);
-        //System.out.println(sleepAnalysisResult7);
         Logger.log(" " + sleepAnalysisResult7);
 
         System.out.println("________________ SleepAnalysisResult, Сhronotype");
         Сhronotype chronotype = new Сhronotype();
         SleepAnalysisResult sleepAnalysisResult8 = chronotype.apply(listSession);
-        //System.out.println(sleepAnalysisResult8);
         Logger.log("Согласно хронотипу " + sleepAnalysisResult8);
     }
 }
